da95d3c6ac257b631800febd2ebbb3efb498f861
Dasom Kim <llsomll90@gmail.com>
2025-06-06 14:57:00 -0400

    profile photo added

---
+++ src/hooks/useGetCurrentUserProfile.ts
@@ -0,0 +1,15 @@
+import { useQuery, UseQueryResult } from "@tanstack/react-query"
+import { getCurrentUserProfile } from "../apis/userApi"
+import { User } from "../models/user";
+
+const useGetCurrentUserProfile = (): UseQueryResult<User, Error> => {
+    const accessToken = localStorage.getItem('access_token');
+
+    return useQuery({
+        queryKey: ['current-user-profile'],
+        queryFn: getCurrentUserProfile,
+        enabled: !!accessToken, // Only run the query if access token is available
+    })
+}
+
+export default useGetCurrentUserProfile;
--- src/layout/components/Navbar.tsx
+++ src/layout/components/Navbar.tsx
@@ -1,11 +1,31 @@
-import { Box } from '@mui/material'
+import { Box, styled, Avatar, Typography } from '@mui/material'
 import React from 'react'
 import LoginButton from '../../common/components/LoginButton'
+import useGetCurrentUserProfile from '../../hooks/useGetCurrentUserProfile'
+
+const UserProfileContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  gap: theme.spacing(1),
+}));
 
 const Navbar = () => {
+  const {data: userProfile} = useGetCurrentUserProfile();
+
+
   return (
     <Box display="flex" justifyContent="flex-end" alignItems="center" height="64px">
-      <LoginButton />
+      { userProfile? 
+        <UserProfileContainer>
+          <Typography variant="body1">Welcome, {userProfile.display_name}!</Typography>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src={userProfile.images[0]?.url}
+            alt={userProfile.display_name}>
+          </Avatar>
+        </UserProfileContainer>
+        : <LoginButton />}
     </Box>
   )
 }
